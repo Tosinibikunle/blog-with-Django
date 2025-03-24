e7064bd03a1094ff4e7f316838044cc660be65c8
TosinIbikunle <tosinibikunle1@gmail.com>
2025-03-24 11:18:01 +0100

    blog creation

--- MyBlog/MyBlog/urls.py
+++ MyBlog/MyBlog/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for inventorymanagement project.
+URL configuration for MyBlog project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
