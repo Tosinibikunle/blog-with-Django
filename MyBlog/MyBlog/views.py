a8e173d8c650dba6f098a907c8d17efb1dd9bd7d
TosinIbikunle <tosinibikunle1@gmail.com>
2025-03-25 15:55:26 +0100

    templates

--- MyBlog/MyBlog/views.py
+++ MyBlog/MyBlog/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def Homepage(request):
-    return HttpResponse('Homepage')
+    # return HttpResponse('Homepage')
+    return render(request, 'homepage.html')
 
 def about(request):
-    return HttpResponse('about')
+    # return HttpResponse('about')
+    return render(request, 'about.html')
 
