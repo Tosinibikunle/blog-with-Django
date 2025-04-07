cb9d974d7c9058a67197fb5e749fabf2a350add0
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-04-07 07:15:08 +0000

    acct views setup

--- MyBlog/accounts/views.py
+++ MyBlog/accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .import views 
+# Create your vi.
 
-# Create your views here.
+def signup_view(request):
+    return render(request,'accounts/signup_view.html',name='signup')
+
+
+def login_view(request):
+    return render(request, 'accounts/login_view.html',name='login')
+    
