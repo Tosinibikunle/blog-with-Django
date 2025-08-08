8809af6b24ff9fd84e14d1225503357ed0cc31e0
TosinIbikunle <tosinibikunle1@gmail.com>
2025-08-08 14:47:45 +0100

    formattinng and clean up v2

--- MyBlog/MyBlog/views.py
+++ MyBlog/MyBlog/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 
 
 def Homepage(request):
-    # return HttpResponse('Homepage')
-    return render(request, 'homepage.html')
+ 
+    return render(request, "homepage.html")
 
 
 def about(request):
-    # return HttpResponse('about')
-    return render(request, 'about.html')
+  
+    return render(request, "about.html")
