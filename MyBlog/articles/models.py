6cc8ee50c369e6cc26d9362dbd96d3155b1448fa
Tosinibikunle <tosinibikunle1@gmail.com>
2025-08-11 12:04:03 +0100

    articles models update

--- MyBlog/articles/models.py
+++ MyBlog/articles/models.py
@@ -1,15 +1,31 @@
 from django.db import models
-
+from django.utils.text import slugify
 
 class Article(models.Model):
+    CATEGORY_CHOICES = [
+        ("tech", "Technology"),
+        ("lifestyle", "Lifestyle"),
+        ("business", "Business"),
+        ("news", "News"),
+    ]
+
     title = models.CharField(max_length=100)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True, blank=True)  # Ensure unique slugs
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)  # For edits
     thumb = models.ImageField(default='default.png', blank=True)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, blank=True)
+    is_published = models.BooleanField(default=True)  # For filtering in search/archive
 
     def __str__(self):
         return self.title
 
     def snippet(self):
-        return self.body[:50]
+        return self.body[:50] + "..." if len(self.body) > 50 else self.body
+
+    def save(self, *args, **kwargs):
+        # Auto-generate slug from title if not set
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
