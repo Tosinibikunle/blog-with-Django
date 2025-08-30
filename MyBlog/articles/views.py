3965b1fe289a0675b4953a0caf81cee4aac139b5
Tosinibikunle <tosinibikunle1@gmail.com>
2025-08-30 20:53:14 +0100

    views and tests modified

--- MyBlog/articles/views.py
+++ MyBlog/articles/views.py
@@ -11,3 +11,15 @@ def article_list(request):
 def article_detail(request, slug):
     article = Article.objects.get(slug=slug)
     return render(request, "articles/article_detail.html", {"article": article})
+
+# Placeholder views for additional routes
+def article_create(request):
+    return HttpResponse("Create Article - To be implemented")
+def article_edit(request, slug):
+    return HttpResponse(f"Edit Article {slug} - To be implemented")
+def article_delete(request, slug):
+    return HttpResponse(f"Delete Article {slug} - To be implemented")
+def article_by_category(request, category):
+    return HttpResponse(f"Articles in Category {category} - To be implemented")
+def article_search(request):
+    return HttpResponse("Search Articles - To be implemented")
