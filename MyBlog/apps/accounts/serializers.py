b452b3938cb5ca89da5538201f8d425a65dd4d4a
Tosinibikunle <tosinibikunle1@gmail.com>
2025-09-03 11:16:50 +0100

    accounts app 1.1

--- MyBlog/apps/accounts/serializers.py
+++ MyBlog/apps/accounts/serializers.py
@@ -1,4 +1,67 @@
-
 from .models import customReader, CustomBlogger
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as lazy
+from django.contrib.auth.password_validation import validate_password
+from django.core.validators import RegexValidator
+
+
+User = get_user_model()
+class CustomBloggerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomBlogger
+        fields = ['id', 'email', 'username', 'first_name', 'last_name', 'phone_number', 'date_of_birth', 'bio', 'profile_picture', 'is_verified', 'last_login_ip', 'created_at', 'updated_at']
+        read_only_fields = ['is_verified', 'last_login_ip', 'created_at', 'updated_at']
+
+class CustomBloggerCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomBlogger
+        fields = ['email', 'username', 'first_name', 'last_name', 'phone_number', 'date_of_birth', 'bio', 'profile_picture', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True, 'required': True, 'validators': [validate_password]},
+        }
+    def create(self, validated_data):
+        user = CustomBlogger.objects.create(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            phone_number=validated_data.get('phone_number', None),
+            date_of_birth=validated_data.get('date_of_birth', None),
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None),
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+class CustomReaderSerializer(serializers.ModelSerializer):
+    user = CustomBloggerSerializer(read_only=True)
+    class Meta:
+        model = customReader
+        fields = ['id', 'user', 'favorite_genres', 'subscribed_newsletter', 'created_at', 'updated_at']
+        read_only_fields = ['created_at', 'updated_at']
+
+class CustomReaderCreateSerializer(serializers.ModelSerializer):
+    user_id = serializers.PrimaryKeyRelatedField(queryset=CustomBlogger.objects.all(), source='user', write_only=True)
+    class Meta:
+        model = customReader
+        fields = ['user_id', 'favorite_genres', 'subscribed_newsletter']
+    def create(self, validated_data):
+        reader = customReader.objects.create(
+            user=validated_data['user'],
+            favorite_genres=validated_data.get('favorite_genres', ''),
+            subscribed_newsletter=validated_data.get('subscribed_newsletter', False),
+        )
+        reader.save()
+        return reader
+    def validate_favorite_genres(self, value):
+        if value and not all(genre.isalpha() or genre.isspace() or genre == ',' for genre in value):
+            raise serializers.ValidationError(lazy("Favorite genres must only contain letters, spaces, and commas."))
+        return value
+    def validate(self, attrs):
+        user = attrs.get('user')
+        if customReader.objects.filter(user=user).exists():
+            raise serializers.ValidationError(lazy("This user already has a reader profile."))
+        return attrs
+
--- MyBlog/apps/accounts/views.py
+++ MyBlog/apps/accounts/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets, permissions
+from .models import CustomBlogger, customReader
+from .serializers import CustomBloggerSerializer, CustomBloggerCreateSerializer, CustomReaderSerializer, CustomReaderCreateSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+User = get_user_model()
+
+class CustomBloggerViewSet(viewsets.ModelViewSet):
+    queryset = CustomBlogger.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return CustomBloggerCreateSerializer
+        return CustomBloggerSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    def perform_create(self, serializer):
+        serializer.save()
+
+class CustomReaderViewSet(viewsets.ModelViewSet):
+    queryset = customReader.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return CustomReaderCreateSerializer
+        return CustomReaderSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    def perform_create(self, serializer):
+        serializer.save()
+
+class CustomBloggerDetailViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = CustomBlogger.objects.all()
+    serializer_class = CustomBloggerSerializer
+    permission_classes = [permissions.IsAuthenticated]
