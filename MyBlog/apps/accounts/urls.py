6e590e9f79b7514d94f51087f5b46288a062beed
Tosinibikunle <tosinibikunle1@gmail.com>
2025-09-04 12:05:35 +0100

    accounts app v1.3

--- MyBlog/apps/accounts/urls.py
+++ MyBlog/apps/accounts/urls.py
@@ -1,2 +1,17 @@
-from .views import customReader, CustomBlogger
-from .serializers import CustomReaderCreateSerializer, CustomBloggerSerializer
+
+from django.urls import path, include
+from django.contrib import admin
+from .models import CustomBlogger, customReader
+from rest_framework.routers import DefaultRouter
+from .views import CustomBloggerViewSet, CustomReaderViewSet, CustomBloggerDetailViewSet
+
+router = DefaultRouter()
+router.register(r'bloggers', CustomBloggerViewSet)
+router.register(r'readers', CustomReaderViewSet)
+router.register(r'blogger-detail', CustomBloggerDetailViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+
