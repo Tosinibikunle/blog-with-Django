ddce638e2f8070cb3de6fe40ff684e5c8a1179de
TosinIbikunle <tosinibikunle1@gmail.com>
2025-09-05 16:50:50 +0100

    first server run attempt

--- MyBlog/apps/accounts/urls.py
+++ MyBlog/apps/accounts/urls.py
@@ -6,9 +6,9 @@ from rest_framework.routers import DefaultRouter
 from .views import CustomBloggerViewSet, CustomReaderViewSet, CustomBloggerDetailViewSet
 
 router = DefaultRouter()
-router.register(r'bloggers', CustomBloggerViewSet)
-router.register(r'readers', CustomReaderViewSet)
-router.register(r'blogger-detail', CustomBloggerDetailViewSet)
+router.register(r'bloggers', CustomBloggerViewSet, basename='bloggers')
+router.register(r'readers', CustomReaderViewSet, basename='readers')
+router.register(r'blogger-detail', CustomBloggerDetailViewSet, basename='blogger-detail')
 
 urlpatterns = [
     path('', include(router.urls)),
