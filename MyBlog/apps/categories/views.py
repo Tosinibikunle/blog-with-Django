bada68b54cb9cdbd73c3421fc5ffbf8f9ae57d64
TosinIbikunle <tosinibikunle1@gmail.com>
2025-09-06 21:39:42 +0100

    categories view mods

--- MyBlog/apps/categories/views.py
+++ MyBlog/apps/categories/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from.models import Category, Tag
 # Create your views here.
+
+class CategoryViewset():
+    def list(self, request):
+        categories = Category.objects.all()
+        return render(request, 'categories/category_list.html', {'categories': categories})
+
+class TagViewset():
+    def list(self, request):
+        tags = Tag.objects.all()
+        return render(request, 'categories/tag_list.html', {'tags': tags})
