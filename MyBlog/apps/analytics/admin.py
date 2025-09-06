86c696b0faaf63e6e463a769c27e8782645ff8b6
TosinIbikunle <tosinibikunle1@gmail.com>
2025-09-06 21:35:44 +0100

    categories model

--- MyBlog/apps/analytics/admin.py
+++ MyBlog/apps/analytics/admin.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 from .models import PageView, ClickEvent
 
 
-# Register your models here.
 admin.site.register(PageView)
 admin.site.register(ClickEvent)
--- MyBlog/apps/categories/models.py
+++ MyBlog/apps/categories/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
