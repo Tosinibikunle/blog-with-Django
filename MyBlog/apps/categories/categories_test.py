0eaae888a4ab03b160d21dd701bebd001154cfa4
TosinIbikunle <tosinibikunle1@gmail.com>
2025-09-09 20:39:55 +0100

    categories_test.py creation

--- MyBlog/apps/categories/categories_test.py
+++ MyBlog/apps/categories/categories_test.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Category, Tag
+
 
 # Create your tests here.
+
+class CategoryModelTest():
+    def setup(self):
+        self.category = Category.objects.create(name='Django', slug='django')
+
+    def test_category_creation(self):
+        assert self.category.name == 'Django'
+        assert self.category.slug == 'django'
+        assert str(self.category) == 'Django'
+        assert str(self.category.slug) == 'django'
