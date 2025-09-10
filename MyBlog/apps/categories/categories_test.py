a936e3fabb692d0f0ddc7f74ced39ef294dcee42
Tosinibikunle <tosinibikunle1@gmail.com>
2025-09-10 14:46:36 +0100

    categories app format

--- MyBlog/apps/categories/categories_test.py
+++ MyBlog/apps/categories/categories_test.py
@@ -1,15 +1,16 @@
 from django.test import TestCase
-from .models import Category, Tag
+from .models import Category
 
 
-# Create your tests here.
 
-class CategoryModelTest():
+
+
+class CategoryModelTest(TestCase):
     def setup(self):
-        self.category = Category.objects.create(name='Django', slug='django')
+        self.category = Category.objects.create(name="Django", slug="django")
 
     def test_category_creation(self):
-        assert self.category.name == 'Django'
-        assert self.category.slug == 'django'
-        assert str(self.category) == 'Django'
-        assert str(self.category.slug) == 'django'
+        assert self.category.name == "Django"
+        assert self.category.slug == "django"
+        assert str(self.category) == "Django"
+        assert str(self.category.slug) == "django"
