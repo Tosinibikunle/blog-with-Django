080e3f98a7ac3adee0922f7534b44d7932dbd398
Tosinibikunle <tosinibikunle1@gmail.com>
2025-09-14 16:24:15 +0100

    debugging 1.2

--- MyBlog/MyBlog/urls.py
+++ MyBlog/MyBlog/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", TemplateView.as_view(template_name="Homepage.html"), name="home"),
 
-    # Example app routes
+   
     path("accounts/", include("apps.accounts.urls")),
     path("posts/", include("apps.posts.urls")),
     path("comments/", include("apps.comments.urls")),
