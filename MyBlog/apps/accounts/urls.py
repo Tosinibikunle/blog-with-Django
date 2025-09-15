fd1ab9dfda74f6539176624622998fc7dcde5022
Tosinibikunle <tosinibikunle1@gmail.com>
2025-09-15 21:21:56 +0100

    debugging v1.5

--- MyBlog/apps/accounts/urls.py
+++ MyBlog/apps/accounts/urls.py
@@ -1,17 +1,14 @@
-
 from django.urls import path, include
-from django.contrib import admin
-# from .models import CustomBlogger, customReader
 from rest_framework.routers import DefaultRouter
 from .views import CustomBloggerViewSet, CustomReaderViewSet, CustomBloggerDetailViewSet
 
 router = DefaultRouter()
-router.register(r'bloggers', CustomBloggerViewSet, basename='bloggers')
-router.register(r'readers', CustomReaderViewSet, basename='readers')
-router.register(r'blogger-detail', CustomBloggerDetailViewSet, basename='blogger-detail')
+router.register(r"bloggers", CustomBloggerViewSet, basename="bloggers")
+router.register(r"readers", CustomReaderViewSet, basename="readers")
+router.register(
+    r"blogger-detail", CustomBloggerDetailViewSet, basename="blogger-detail"
+)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
 ]
-
-
