302f6b6565ee871d95feab920a11936242b84817
TosinIbikunle <tosinibikunle1@gmail.com>
2025-09-17 18:41:56 +0100

    working on likes app v1.1

--- MyBlog/apps/likes/views.py
+++ MyBlog/apps/likes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Like
 
 # Create your views here.
+class LikeViewSet(viewsets.ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
