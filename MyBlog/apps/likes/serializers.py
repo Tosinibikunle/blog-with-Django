d5b980dd0da277d00758e8eb035a265b1cfeac52
Tosinibikunle <tosinibikunle1@gmail.com>
2025-09-17 18:47:35 +0100

    working on likes app v1.2

---
+++ MyBlog/apps/likes/serializers.py
@@ -0,0 +1,27 @@
+from .models import Like
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class LikeSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
+    class Meta:
+        model = Like
+        fields = ['id', 'user', 'post', 'created_at']
+        read_only_fields = ['created_at']
+
+class LikeCreateSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
+    class Meta:
+        model = Like
+        fields = ['user', 'post']
+
+    def validate(self, data):
+        user = data.get('user')
+        post = data.get('post')
+        if Like.objects.filter(user=user, post=post).exists():
+            raise serializers.ValidationError("You have already liked this post.")
+        return data
--- MyBlog/apps/likes/views.py
+++ MyBlog/apps/likes/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Like
+from .serializers import LikeSerializer, LikeCreateSerializer
 
 # Create your views here.
 class LikeViewSet(viewsets.ModelViewSet):
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return LikeCreateSerializer
+        return super().get_serializer_class()
