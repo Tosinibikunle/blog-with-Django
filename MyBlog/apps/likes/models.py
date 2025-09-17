b8385544f259ea5f88248676fbcdc54ae6d7c4bb
TosinIbikunle <tosinibikunle1@gmail.com>
2025-09-17 18:47:35 +0100

    working on likes app v1.2

--- MyBlog/apps/likes/models.py
+++ MyBlog/apps/likes/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+    
+
+User = get_user_model()
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey('posts.Post', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
+        ordering = ['-created_at']
+        verbose_name = "like"
+
+    def __str__(self):
+        return f"{self.user.email} liked post {self.post.id}"
+    
+
