657b014f66cd3a2b9d5a9fe7364be7d23f98343b
Tosinibikunle <tosinibikunle1@gmail.com>
2025-09-18 16:28:29 +0100

    likes app debugging

--- MyBlog/apps/likes/urls.py
+++ MyBlog/apps/likes/urls.py
@@ -1,5 +1,4 @@
-from django.urls import path
-# from django.utils.translation import gettext_lazy as lazy
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
@@ -7,10 +6,8 @@ router = DefaultRouter()
 router.register(r'likes', views.LikeViewSet, basename='like')
 
 urlpatterns = [
-    path('like/', views.LikeCreateViewSet.as_view(), name='like-create'),
-    path('unlike/', views.LikeDeleteView.as_view(), name='like-delete'),
+    path('unlike/<int:pk>/', views.LikeDeleteView.as_view(), name='like-delete'),
     path('likes/<int:post_id>/', views.LikeListViewSet.as_view(), name='like-list'),
     path('likes/count/<int:post_id>/', views.LikeCountView.as_view(), name='like-count'),
+    path('', include(router.urls)),  # include router-generated routes
 ]
-
-urlpatterns += router.urls
--- MyBlog/apps/likes/views.py
+++ MyBlog/apps/likes/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Like
 from .serializers import LikeSerializer, LikeCreateSerializer
+from rest_framework.generics import DestroyAPIView, ListAPIView
+from rest_framework.response import Response
 
 # Create your views here.
 class LikeViewSet(viewsets.ModelViewSet):
@@ -20,16 +22,28 @@ class LikeCreateViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save()
 
-class LikeDeleteView(viewsets.ModelViewSet):
+class LikeDeleteView(DestroyAPIView):
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
 
     def perform_destroy(self, instance):
         instance.delete()
 
-class LikeListViewSet(viewsets.ModelViewSet):
+class LikeListViewSet(ListAPIView):
     serializer_class = LikeSerializer
 
     def get_queryset(self):
         post_id = self.kwargs['post_id']
-        return Like.objects.filter(post_id=post_id)        
+        return Like.objects.filter(post_id=post_id)
+
+class LikeCountView(ListAPIView):
+    serializer_class = LikeSerializer
+
+    def get_queryset(self):
+        post_id = self.kwargs['post_id']
+        return Like.objects.filter(post_id=post_id)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        count = queryset.count()
+        return Response({'post_id': self.kwargs['post_id'], 'like_count': count})
