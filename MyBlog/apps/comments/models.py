06da7421eb028db152b417ff3d6694fc81e9424f
Tosinibikunle <tosinibikunle1@gmail.com>
2025-09-20 13:08:46 +0100

    comment app init

--- MyBlog/apps/comments/models.py
+++ MyBlog/apps/comments/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
+# from django.utils.translation import gettext_lazy as lazy
 
+
+# user = settings.AUTH_USER_MODEL
 # Create your models here.
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+    post = models.ForeignKey(
+        "posts.Post",
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "comment"
+        verbose_name_plural = "comments"
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"Comment by {self.user} on {self.post}"
---
+++ MyBlog/apps/comments/serializers.py
@@ -0,0 +1,7 @@
+from .models import Comment
+from rest_framework import serializers
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'user', 'post', 'content', 'created_at']
