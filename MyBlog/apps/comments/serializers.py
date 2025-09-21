d22cbf13fcd71c9f2949b5798ecbf5fe10517c5e
TosinIbikunle <tosinibikunle1@gmail.com>
2025-09-21 12:58:43 +0100

    comments app - add comment model, serializer, view

--- MyBlog/apps/comments/serializers.py
+++ MyBlog/apps/comments/serializers.py
@@ -5,3 +5,36 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ['id', 'user', 'post', 'content', 'created_at']
+        read_only_fields = ['id', 'created_at']
+
+    def create(self, validated_data):
+        return Comment.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.content = validated_data.get('content', instance.content)
+        instance.save()
+        return instance
+
+class CommentDetailSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+    post = serializers.StringRelatedField()
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'user', 'post', 'content', 'created_at']
+        read_only_fields = ['id', 'user', 'post', 'created_at']    
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['user'] = instance.user.username
+        representation['post'] = instance.post.title
+        return representation
+    
+class CommentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['post', 'content']
+    
+    def create(self, validated_data):
+        user = self.context['request'].user
+        return Comment.objects.create(user=user, **validated_data)
--- MyBlog/apps/comments/views.py
+++ MyBlog/apps/comments/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer, CommentCreateSerializer
+from rest_framework import generics, permissions
 
 # Create your views here.
+
+class CommentListCreateView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return CommentCreateSerializer
+        return CommentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
