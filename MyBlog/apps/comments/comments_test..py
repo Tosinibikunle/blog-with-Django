bb84cf83bea91432407e70516e591b14e00c9934
TosinIbikunle <tosinibikunle1@gmail.com>
2025-09-22 21:27:04 +0100

    comments app improvements

--- MyBlog/apps/comments/comments_test..py
+++ MyBlog/apps/comments/comments_test..py
@@ -5,16 +5,21 @@ from apps.posts.models import Post
 
 User = get_user_model()
 
+
 class CommentModelTest(TestCase):
 
     def setUp(self):
-       
-        self.user = User.objects.create_user(username='testuser', password='testpass')
-        self.post = Post.objects.create(title='Test Post', content='Test Content', author=self.user)
+
+        self.user = User.objects.create_user(username="testuser", password="testpass")
+        self.post = Post.objects.create(
+            title="Test Post", content="Test Content", author=self.user
+        )
 
     def test_comment_creation(self):
-        comment = Comment.objects.create(user=self.user, post=self.post, content='Test Comment')
+        comment = Comment.objects.create(
+            user=self.user, post=self.post, content="Test Comment"
+        )
         self.assertEqual(comment.user, self.user)
         self.assertEqual(comment.post, self.post)
-        self.assertEqual(comment.content, 'Test Comment')
+        self.assertEqual(comment.content, "Test Comment")
         self.assertIsNotNone(comment.created_at)
