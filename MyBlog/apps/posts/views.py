d767ddb7e529ba4870738086b0fd50eeafb2ada1
TosinIbikunle <tosinibikunle1@gmail.com>
2025-09-24 19:24:16 +0100

    post app in progress

--- MyBlog/apps/posts/views.py
+++ MyBlog/apps/posts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
+
+class PostListView:
+    def list(self, request):
+        posts = Post.objects.all()
+        return render(request, 'posts/post_list.html', {'posts': posts})
+    def detail(self, request, post_id):
+        post = Post.objects.get(id=post_id)
+        return render(request, 'posts/post_detail.html', {'post': post})
+    def author_posts(self, request, author_id):
+        posts = Post.objects.filter(author_id=author_id)
+        return render(request, 'posts/author_posts.html', {'posts': posts})
+    def author_name(self, request, author_name):
+        posts = Post.objects.filter(author__name=author_name)
+        return render(request, 'posts/author_posts.html', {'posts': posts})
+    def author_email(self, request, author_email):
+        posts = Post.objects.filter(author__email=author_email)
+        return render(request, 'posts/author_posts.html', {'posts': posts})
