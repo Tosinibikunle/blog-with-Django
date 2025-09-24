5c4a7c02fdb9d1dca6b5c66f1d7056d6558539cf
TosinIbikunle <tosinibikunle1@gmail.com>
2025-09-24 19:27:46 +0100

    post app work in progress

--- MyBlog/apps/posts/urls.py
+++ MyBlog/apps/posts/urls.py
@@ -1,3 +1,10 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('posts/', views.PostListView.as_view(), name='post_list'),
+    path('posts/<int:post_id>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('posts/author/<int:author_id>/', views.PostAuthorView.as_view(), name='author_posts'),
+    path('posts/author/name/<str:author_name>/', views.PostAuthorView.as_view(), name='author_name_posts'),
+    path('posts/author/email/<str:author_email>/', views.PostAuthorView.as_view(), name='author_email_posts'),
+]
