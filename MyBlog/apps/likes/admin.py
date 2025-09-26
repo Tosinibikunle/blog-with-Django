d6565e5986678b9e3017dd452731db73a63560be
TosinIbikunle <tosinibikunle1@gmail.com>
2025-09-26 15:16:51 +0100

    debugging v2.0

--- MyBlog/apps/likes/admin.py
+++ MyBlog/apps/likes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Like, User
+from .models import Like
 
 admin.site.register(Like)
-admin.site.register(User)
+# admin.site.register(User)
--- MyBlog/apps/posts/views.py
+++ MyBlog/apps/posts/views.py
@@ -1,29 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import View
 from .models import Post
 
 
 
 
-class PostListView:
-    def list(self, request):
+class PostListView(View):
+    def get(self, request):
         posts = Post.objects.all()
         return render(request, 'posts/post_list.html', {'posts': posts})
-    def detail(self, request, post_id):
-        post = Post.objects.get(id=post_id)
-        return render(request, 'posts/post_detail.html', {'post': post})
-    def author_posts(self, request, author_id):
-        posts = Post.objects.filter(author_id=author_id)
-        return render(request, 'posts/author_posts.html', {'posts': posts})
-    def author_name(self, request, author_name):
-        posts = Post.objects.filter(author__name=author_name)
-        return render(request, 'posts/author_posts.html', {'posts': posts})
-    def author_email(self, request, author_email):
-        posts = Post.objects.filter(author__email=author_email)
-        return render(request, 'posts/author_posts.html', {'posts': posts})
-    
-class PostDetailView:
+
+class PostDetailView(View):
     def get(self, request, post_id):
-        post = Post.objects.get(id=post_id)
+        post = get_object_or_404(Post, id=post_id)
         return render(request, 'posts/post_detail.html', {'post': post})
     def get_author_posts(self, request, author_id):
         posts = Post.objects.filter(author_id=author_id)
@@ -31,9 +20,14 @@ class PostDetailView:
     def get_author_name_posts(self, request, author_name):
         posts = Post.objects.filter(author__name=author_name)
         return render(request, 'posts/author_posts.html', {'posts': posts})    
-class PostAuthorView:
-    def get_posts_by_author_id(self, request, author_id):
-        posts = Post.objects.filter(author_id=author_id)
+class PostAuthorView(View):
+    def get(self, request, author_id=None, author_name=None):
+        if author_id:
+            posts = Post.objects.filter(author_id=author_id)
+        elif author_name:
+            posts = Post.objects.filter(author__name=author_name)
+        else:
+            posts = Post.objects.none()
         return render(request, 'posts/author_posts.html', {'posts': posts})
     def get_posts_by_author_name(self, request, author_name):
         posts = Post.objects.filter(author__name=author_name)
