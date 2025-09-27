c632cf175f32003db771763e8015006022bafc43
TosinIbikunle <tosinibikunle1@gmail.com>
2025-09-27 18:14:03 +0100

    debugging v2.2

--- MyBlog/apps/posts/views.py
+++ MyBlog/apps/posts/views.py
@@ -14,12 +14,16 @@ class PostDetailView(View):
     def get(self, request, post_id):
         post = get_object_or_404(Post, id=post_id)
         return render(request, 'posts/post_detail.html', {'post': post})
+    
     def get_author_posts(self, request, author_id):
         posts = Post.objects.filter(author_id=author_id)
         return render(request, 'posts/author_posts.html', {'posts': posts})
+    
     def get_author_name_posts(self, request, author_name):
         posts = Post.objects.filter(author__name=author_name)
-        return render(request, 'posts/author_posts.html', {'posts': posts})    
+        return render(request, 'posts/author_posts.html', {'posts': posts})  
+    
+      
 class PostAuthorView(View):
     def get(self, request, author_id=None, author_name=None):
         if author_id:
