081c61b8d064f87b9dbc24c0535b05a5d8e3afea
TosinIbikunle <tosinibikunle1@gmail.com>
2025-10-01 21:36:26 +0100

    routes correction in posts app

--- MyBlog/apps/posts/urls.py
+++ MyBlog/apps/posts/urls.py
@@ -6,9 +6,9 @@ router = DefaultRouter()
 router.register(r'posts', views.PostListAPIView, basename='post')
 
 urlpatterns = [
-    path('/', views.PostListView.as_view(), name='post_list'),
-    path('/<int:post_id>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('/author/<int:author_id>/', views.PostAuthorView.as_view(), name='author_posts'),
-    path('/author/name/<str:author_name>/', views.PostAuthorView.as_view(), name='author_name_posts'),
-    path('/author/email/<str:author_email>/', views.PostAuthorView.as_view(), name='author_email_posts'),
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('<int:post_id>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('author/<int:author_id>/', views.PostAuthorView.as_view(), name='author_posts'),
+    path('author/name/<str:author_name>/', views.PostAuthorView.as_view(), name='author_name_posts'),
+    path('author/email/<str:author_email>/', views.PostAuthorView.as_view(), name='author_email_posts'),
 ]
