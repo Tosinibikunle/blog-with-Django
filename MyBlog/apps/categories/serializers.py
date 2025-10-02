77d1ce003300d19d12174a00556a5e9eb51c90d8
TosinIbikunle <tosinibikunle1@gmail.com>
2025-10-02 22:00:23 +0100

    fix: corrected the import statement for CategorySerializer in views.py

--- MyBlog/apps/categories/serializers.py
+++ MyBlog/apps/categories/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from .models import Category, Tag
 
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = "__all__"
+
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
--- MyBlog/apps/categories/views.py
+++ MyBlog/apps/categories/views.py
@@ -2,11 +2,12 @@ from rest_framework import viewsets
 from .models import Category, Tag
 from .serializers import CategorySerializer, TagSerializer
 
+
 class CategoryViewset(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+
 class TagViewset(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
-
