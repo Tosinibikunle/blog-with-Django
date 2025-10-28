fa40f335d0e0e0a04ceb2d4cb47dd18d039b24a9
TosinIbikunle <tosinibikunle1@gmail.com>
2025-10-28 19:55:16 +0100

    debugging analytics routes

--- MyBlog/apps/analytics/urls.py
+++ MyBlog/apps/analytics/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('page-views/', views.PageViewList.as_view(), name='page-view-list'),
+    path('page-views/', views.PageViewList.as_view(), name='page-view'),
     path('click-events/', views.ClickEventList.as_view(), name='click-event-list'),
     path('page-views/<int:pk>/', views.PageViewDetail.as_view(), name='page-view-detail'),
     path('click-events/<int:pk>/', views.ClickEventDetail.as_view(), name='click-event-detail'),]
